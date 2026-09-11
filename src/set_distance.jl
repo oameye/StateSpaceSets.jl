154597a4b72fb573b4b4c9582deebc923e74f251
Datseris <datseris.george@gmail.com>
2026-09-11 08:14:40 +0100

    remove unecessary `static` reference

--- src/set_distance.jl
+++ src/set_distance.jl
@@ -27,7 +27,7 @@ A distance that can be used in [`set_distance`](@ref).
 The `Centroid` method returns the distance (according to `metric`) between the
 [centroids](https://en.wikipedia.org/wiki/Centroid) (a.k.a. centers of mass) of the sets.
 
-`metric` can be any function that takes in two static vectors are returns a positive
+`metric` can be any function that takes in two vectors are returns a positive
 definite number to use as a distance (and typically is a `Metric` from Distances.jl).
 """
 struct Centroid{M}
